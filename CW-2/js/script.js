2e62a9e98f9d53646ea2d76d5a8624f827871891
Beka-bit <b6134991@gmail.com>
2024-10-31 01:49:38 +0600

    Initialize HW-Variables-and-Data-types-day_3

---
+++ CW-2/js/script.js
@@ -0,0 +1,33 @@
+let name = "Bekarys";
+
+console.log("My name: " + name + "/ Type: " + typeof name);
+
+
+const yerth = 16;
+
+console.log("My yerth: " + yerth + "/ Type: " + typeof yerth);
+
+
+let score = false
+if (true) {
+    let score = true
+    console.log( "His a Student?: " + score +  "/ Type: " + typeof score);
+    
+}
+
+
+var age = "Жасы: "
+var age = "25"
+console.log("Жасы: " + age + "/ Type: " + typeof age);
+
+
+var age = "Жасы: "
+var age = "2"
+var age = "5"
+console.log("Жасы: " + "2" + "5" +  "/ Type: " + typeof age);
+
+
+
+
+
+
---
+++ HW-2/js/type.js
@@ -0,0 +1,4 @@
+console.log('Hi, world in Javascript');
+
+console.log('what is that?');
+
